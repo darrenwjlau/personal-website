486cc65b00de75ee59d51d34acf8c77bd95ebb6a
Darren Lau Wei Jie <darrenwjlau@gmail.com>
2026-03-15 12:05:56 +0800

    Update AboutStore.ts
    Update age

--- src/stores/AboutStore.ts
+++ src/stores/AboutStore.ts
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia'
 export const useAboutStore = defineStore('AboutStore', {
     state: () => ({
-      about: "I am a 17 year old student from Malaysia that likes to create interesting projects."
+      about: "I am a 18 year old from Malaysia that likes to create interesting projects."
      }),
      getters: {
         about_message: (state) => state.about
